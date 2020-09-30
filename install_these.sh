3eb726a5c8ee638a920f5929a7be688cf65be377
Vickus Botha <vickusbotha@pop-os.localdomain>
2020-09-30 19:23:57 +0200

    first

---
+++ install_these.sh
@@ -0,0 +1,7 @@
+sudo apt-get install libssl-dev 
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libmariadbclient-dev 
+sudo apt-get install libmariadb-client-lgpl-dev
+sudo apt-get install libudunits2-dev
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install libgdal-dev
